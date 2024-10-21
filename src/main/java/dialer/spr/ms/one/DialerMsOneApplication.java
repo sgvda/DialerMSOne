11f4deb0dbcf4ae20cf093531fd7f6e9446ab0df
Admin <Admin@DESKTOP-KSOO4CR>
2024-10-21 17:04:15 +0530

    BGINING OF THE PROJECT COMMIT ONE AS LEAD

---
+++ src/main/java/dialer/spr/ms/one/DialerMsOneApplication.java
@@ -0,0 +1,13 @@
+package dialer.spr.ms.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialerMsOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DialerMsOneApplication.class, args);
+	}
+
+}
