c578dad3fa30bcd882ff01e035ab08a08207bfbe
Admin <Admin@DESKTOP-KSOO4CR>
2024-10-24 13:36:08 +0530

    tomcat supporting added

--- src/main/java/dialer/spr/ms/one/DialerMsOneApplication.java
+++ src/main/java/dialer/spr/ms/one/DialerMsOneApplication.java
@@ -2,9 +2,10 @@ package dialer.spr.ms.one;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DialerMsOneApplication {
+public class DialerMsOneApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DialerMsOneApplication.class, args);
